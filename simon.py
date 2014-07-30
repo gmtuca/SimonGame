ba6ce251b8136c7655a2e73fc864c7e775b8612b
Arthur Bicalho Ceccotti <gmtuca@gmail.com>
2014-07-30 18:24:52 +0100

    Added header

--- simon.py
+++ simon.py
@@ -1,3 +1,11 @@
+#SimonGame
+#=========
+#Simple Raspberry Pi Simon Memory Game, which requires\n
+#-Raspberry Pi Model B
+#-3 buttons (and 3x 1000Ω resistors - depending on current and voltage)
+#-3 LEDs (and 3x 165Ω resistors - depending on current and voltage)
+#-a few jumper wires -optional: a breadboard
+
 import RPi.GPIO as gpio
 import time
 from random import randint
